2f3a3b34528ae0746e73f37977dc4e7d9b28f35a
Maciej Wilczyński <mwilczynski@cleeng.com>
2018-10-20 11:20:58 +0200

    Improve loading page

--- src/AppOverlay.js
+++ src/AppOverlay.js
@@ -3,7 +3,12 @@ import './AppOverlay.css';
 
 class AppOverlay extends Component {
 	render() {
-        return <div><div className="AppOverlayContainer"><div className="AppOverlay"><div className="OverlayContainer"><div className="spinner"><div className="double-bounce1"></div><div className="double-bounce2"></div></div></div></div></div><div class="hide_while_loading">{this.props.children}</div></div>;
+        return <div><div className="AppOverlayContainer"><div className="AppOverlay"><div className="OverlayContainer"><div class="sk-folding-cube">
+  <div class="sk-cube1 sk-cube"></div>
+  <div class="sk-cube2 sk-cube"></div>
+  <div class="sk-cube4 sk-cube"></div>
+  <div class="sk-cube3 sk-cube"></div>
+</div></div></div></div><div class="hide_while_loading">{this.props.children}</div></div>;
 	}
 }
 
